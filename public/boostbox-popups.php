b0b9298c072a4a032895ef8a1a0a5c3faef4ce62
Robert DeVore <deviodigital@gmail.com>
2024-01-09 16:12:02 -0500

    :ok_hand: IMPROVE: Updated popup to use WP_Query instead of REST API

--- public/boostbox-popups.php
+++ public/boostbox-popups.php
@@ -11,6 +11,11 @@
  * @since      1.0.0
  */
 
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
 /**
  * Popup HTML
  * 
@@ -29,6 +34,9 @@ function boostbox_popup_html() {
     if ( '' == $popup_id ) {
         $popup_id = $settings['boostbox_global_popup'];
     }
+    // Bail early?
+    if ( ! $popup_id || 'popup_disabled' == $popup_id ) { return; }
+
     // Set popup width from popup meta.
     $popup_width = 'max-width: ' . get_post_meta( $popup_id, 'boostbox_display_max_width', true );
     // Set width based on the first block, or defaults as a fallback.
@@ -37,27 +45,12 @@ function boostbox_popup_html() {
     }
     // Get the popup max width (if any).
     $max_width = 'style="' . $popup_width . '"';
-    // Bail early?
-    if ( ! $popup_id || 'popup_disabled' == $popup_id ) { return; }
-    // Get blog post from rest API.
-    $response = wp_remote_get( get_bloginfo( 'url' ) . '/wp-json/wp/v2/popups/' . $popup_id );
-    // Exit if error.
-    if ( is_wp_error( $response ) ) {
-        return;
-    }
-    // Get the body.
-    $popup = json_decode( wp_remote_retrieve_body( $response ) );
     // Popup position.
     $popup_position = get_post_meta( $popup_id, 'boostbox_display_location', true );
     // Popup animation.
     $popup_animation = get_post_meta( $popup_id, 'boostbox_animation_type', true );
-    // Exit if nothing is returned.
-    if ( empty( $popup ) ) {
-        return;
-    }
     // @TODO set custom icon options in the settings for users to choose from.
     $close_icon = apply_filters( 'boostbox_popup_close_icon', '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg>' );
-
     // Popup overlay classes.
     $popup_overlay_classes = apply_filters( 'boostbox_popup_overlay_classes', 'boostbox-popup-overlay' );
     // Popup content classes.
@@ -67,11 +60,37 @@ function boostbox_popup_html() {
     <div class="<?php echo $popup_overlay_classes; ?>">
     <!--Creates the popup content-->
     <div class="<?php echo $popup_content_classes; ?> <?php echo $popup_position . ' ' . $popup_animation; ?>"<?php echo $max_width; ?>>
-        <?php print_r( $popup->content->rendered ); ?>
+        <?php
+        // Query args.
+        $args = array(
+            'post_type'           => 'boostbox_popups',
+            'post_status'         => 'publish',
+            'p'                   => $popup_id,
+            'no_found_rows'       => true,
+            'ignore_sticky_posts' => true
+        );
+
+        // Filter the args.
+        $args = apply_filters( 'boostbox_popup_html_wp_query_args', $args );
+
+        // Build the query.
+        $query = new WP_Query( $args );
+
+        // Check if there are any posts.
+        if ( $query->have_posts() ) {
+            // Loop through the posts.
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                    the_content();
+            }
+            // Restore original post data.
+            wp_reset_postdata();
+        }
+        ?>
         <!--popup's close button-->
         <button class="boostbox-close"><?php print_r( $close_icon ); ?></button>
     </div>
     </div>
     <?php
 }
-add_action( 'wp_footer', 'boostbox_popup_html', 1000 );
+add_action( 'template_redirect', 'boostbox_popup_html' );
