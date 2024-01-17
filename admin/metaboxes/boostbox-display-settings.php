e9db4730fb37a4566d1e958a7a1bfd0d9f2be064
Robert DeVore <deviodigital@gmail.com>
2024-01-17 00:44:26 -0500

    :ok_hand: IMPROVE: Updated display settings metabox with tabbed settings fields

--- admin/metaboxes/boostbox-display-settings.php
+++ admin/metaboxes/boostbox-display-settings.php
@@ -50,192 +50,214 @@ function boostbox_display_settings_metabox_content() {
     echo '<input type="hidden" name="boostbox_display_settings_meta_noncename" id="boostbox_display_settings_meta_noncename" value="' .
     wp_create_nonce( plugin_basename( __FILE__ ) ) . '" />';
 
-    $locations = array(
-        'top-left'      => esc_attr__( 'Top Left', 'boostbox' ),
-        'top-center'    => esc_attr__( 'Top Center', 'boostbox' ),
-        'top-right'     => esc_attr__( 'Top Right', 'boostbox' ),
-        'center-left'   => esc_attr__( 'Center Left', 'boostbox' ),
-        'center-center' => esc_attr__( 'Center Center', 'boostbox' ),
-        'center-right'  => esc_attr__( 'Center Right', 'boostbox' ),
-        'bottom-left'   => esc_attr__( 'Bottom Left', 'boostbox' ),
-        'bottom-center' => esc_attr__( 'Bottom Center', 'boostbox' ),
-        'bottom-right'  => esc_attr__( 'Bottom Right', 'boostbox' ),
-    );
+    echo '<div class="radio-tabs">
 
-    $locations = apply_filters( 'boostbox_display_location_locations', $locations );
+    <input class="state" type="radio" title="General" name="input-state" id="radio1" checked />
+    <input class="state" type="radio" title="Close Button" name="input-state" id="radio2" />
 
-    // Display location.
-    $display_location = get_post_meta( $post->ID, 'boostbox_display_location', true );
+    <div class="tabs">
+        <label for="radio1" id="first-tab" class="tab">
+            <div class="tab-label">' . esc_attr__( 'General', 'boostbox' ) . '</div>
+        </label>
+        <label for="radio2" id="second-tab" class="tab">
+            <div  class="tab-label">' . esc_attr__( 'Close Button', 'boostbox' ) . '</div>
+        </label>
+    </div>
 
-    if ( ! $display_location ) {
-        $display_location = 'center-center';
-    }
-
-    // Select a popup location: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Display location', 'boostbox' ) . '</p>';
-    $field .= '<select id="boostbox_display_location" name="boostbox_display_location">';
-    // Loop through locations.
-    if ( ! empty( $locations ) ) {
-        foreach ( $locations as $location => $value ) {
-            if ( $location == $display_location ) {
-                $selected = 'selected="selected"';
-            } else {
-                $selected = '';
+    <div class="panels">
+        <div id="first-panel" class="panel active animated slideInRight">';
+            $locations = array(
+                'top-left'      => esc_attr__( 'Top Left', 'boostbox' ),
+                'top-center'    => esc_attr__( 'Top Center', 'boostbox' ),
+                'top-right'     => esc_attr__( 'Top Right', 'boostbox' ),
+                'center-left'   => esc_attr__( 'Center Left', 'boostbox' ),
+                'center-center' => esc_attr__( 'Center Center', 'boostbox' ),
+                'center-right'  => esc_attr__( 'Center Right', 'boostbox' ),
+                'bottom-left'   => esc_attr__( 'Bottom Left', 'boostbox' ),
+                'bottom-center' => esc_attr__( 'Bottom Center', 'boostbox' ),
+                'bottom-right'  => esc_attr__( 'Bottom Right', 'boostbox' ),
+            );
+        
+            $locations = apply_filters( 'boostbox_display_location_locations', $locations );
+        
+            // Display location.
+            $display_location = get_post_meta( $post->ID, 'boostbox_display_location', true );
+        
+            if ( ! $display_location ) {
+                $display_location = 'center-center';
             }
-            $field .= '<option value="' . esc_attr( $location ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
-        }
-    }
-    $field .= '</select>';
-    $field .= '</div>';
-
-    echo $field;
-
-    $triggers = array(
-        'auto-open'   => esc_attr__( 'Auto open', 'boostbox' ),
-        'on-scroll'   => esc_attr__( 'On Scroll', 'boostbox' ),
-        'time'        => esc_attr__( 'Time', 'boostbox' ),
-    );
-
-    $triggers = apply_filters( 'boostbox_trigger_type_triggers', $triggers );
-
-    // Popup trigger.
-    $trigger_type = get_post_meta( $post->ID, 'boostbox_trigger_type', true );
-
-    // Select an trigger type: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Trigger type', 'boostbox' ) . '</p>';
-    $field .= '<select id="boostbox_trigger_type" name="boostbox_trigger_type">';
-    // Loop through triggers.
-    if ( ! empty( $triggers ) ) {
-        foreach ( $triggers as $trigger => $value ) {
-            if ( $trigger == $trigger_type ) {
-                $selected = 'selected="selected"';
-            } else {
-                $selected = '';
+        
+            // Select a popup location: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Display location', 'boostbox' ) . '</p>';
+            $field .= '<select id="boostbox_display_location" name="boostbox_display_location">';
+            // Loop through locations.
+            if ( ! empty( $locations ) ) {
+                foreach ( $locations as $location => $value ) {
+                    if ( $location == $display_location ) {
+                        $selected = 'selected="selected"';
+                    } else {
+                        $selected = '';
+                    }
+                    $field .= '<option value="' . esc_attr( $location ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
+                }
             }
-            $field .= '<option value="' . esc_attr( $trigger ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
-        }
-    }
-    $field .= '</select>';
-    $field .= '</div>';
-
-    echo $field;
-
-    // Display speed.
-    $display_speed = get_post_meta( $post->ID, 'boostbox_display_speed', true );
-
-    // Display Speed: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Display speed (milliseconds)', 'boostbox' ) . '</p>';
-    $field .= '<input type="number" name="boostbox_display_speed" value="' . esc_attr( $display_speed ) . '" class="widefat" />';
-    $field .= '</div>';
-
-    echo $field;
-
-    $animations = array(
-        ''            => esc_attr__( '--', 'boostbox' ),
-        'fade-in'     => esc_attr__( 'Fade in', 'boostbox' ),
-        'slide-up'    => esc_attr__( 'Slide up', 'boostbox' ),
-        'slide-down'  => esc_attr__( 'Slide down', 'boostbox' ),
-        'slide-left'  => esc_attr__( 'Slide left', 'boostbox' ),
-        'slide-right' => esc_attr__( 'Slide right', 'boostbox' ),
-        'pop-swirl'   => esc_attr__( 'Pop swirl', 'boostbox' ),
-        'anvil'       => esc_attr__( 'Anvil', 'boostbox' ),
-    );
-
-    $animations = apply_filters( 'boostbox_animation_type_animations', $animations );
-
-    // Popup animation.
-    $popup_animation = get_post_meta( $post->ID, 'boostbox_animation_type', true );
-
-    // Select an animation type: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Animation type', 'boostbox' ) . '</p>';
-    $field .= '<select id="boostbox_animation_type" name="boostbox_animation_type">';
-    // Loop through animations.
-    if ( ! empty( $animations ) ) {
-        foreach ( $animations as $animation => $value ) {
-            if ( $animation == $popup_animation ) {
-                $selected = 'selected="selected"';
-            } else {
-                $selected = '';
+            $field .= '</select>';
+            $field .= '</div>';
+        
+            echo $field;
+        
+            $triggers = array(
+                'auto-open'   => esc_attr__( 'Auto open', 'boostbox' ),
+                'on-scroll'   => esc_attr__( 'On Scroll', 'boostbox' ),
+                'time'        => esc_attr__( 'Time', 'boostbox' ),
+            );
+        
+            $triggers = apply_filters( 'boostbox_trigger_type_triggers', $triggers );
+        
+            // Popup trigger.
+            $trigger_type = get_post_meta( $post->ID, 'boostbox_trigger_type', true );
+        
+            // Select an trigger type: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Trigger type', 'boostbox' ) . '</p>';
+            $field .= '<select id="boostbox_trigger_type" name="boostbox_trigger_type">';
+            // Loop through triggers.
+            if ( ! empty( $triggers ) ) {
+                foreach ( $triggers as $trigger => $value ) {
+                    if ( $trigger == $trigger_type ) {
+                        $selected = 'selected="selected"';
+                    } else {
+                        $selected = '';
+                    }
+                    $field .= '<option value="' . esc_attr( $trigger ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
+                }
             }
-            $field .= '<option value="' . esc_attr( $animation ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
-        }
-    }
-    $field .= '</select>';
-    $field .= '</div>';
-
-    echo $field;
-
-    // Popup animation.
-    $animation_speed = get_post_meta( $post->ID, 'boostbox_animation_speed', true );
-
-    // Select an animation type: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Animation speed (milliseconds)', 'boostbox' ) . '</p>';
-    $field .= '<input type="number" name="boostbox_animation_speed" value="' . esc_attr( $animation_speed ) . '" class="widefat" />';
-    $field .= '</div>';
-
-    echo $field;
-
-    // Max width.
-    $max_width = get_post_meta( $post->ID, 'boostbox_display_max_width', true );
-
-    // Max width: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Max width', 'boostbox' ) . '</p>';
-    $field .= '<input type="text" name="boostbox_display_max_width" value="' . esc_attr( $max_width ) . '" class="widefat" />';
-    $field .= '</div>';
-
-    echo $field;
-
-    $placements = array(
-        'outside' => esc_attr__( 'Outside', 'boostbox' ),
-        'inside'  => esc_attr__( 'Inside', 'boostbox' ),
-        'hidden'  => esc_attr__( 'Hidden', 'boostbox' )
-    );
-
-    $close_icon_placement = apply_filters( 'boostbox_close_icon_placements', $placements );
-
-    // Icon placement.
-    $icon_placement = get_post_meta( $post->ID, 'boostbox_close_icon_placement', true );
-
-    // Select close icon placement: Build the field.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Close icon placement', 'boostbox' ) . '</p>';
-    $field .= '<select id="boostbox_close_icon_placement" name="boostbox_close_icon_placement">';
-    // Loop through animations.
-    if ( ! empty( $close_icon_placement ) ) {
-        foreach ( $close_icon_placement as $placement => $value ) {
-            if ( $placement == $icon_placement ) {
-                $selected = 'selected="selected"';
-            } else {
-                $selected = '';
+            $field .= '</select>';
+            $field .= '</div>';
+        
+            echo $field;
+        
+            // Display speed.
+            $display_speed = get_post_meta( $post->ID, 'boostbox_display_speed', true );
+        
+            // Display Speed: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Display speed (milliseconds)', 'boostbox' ) . '</p>';
+            $field .= '<input type="number" name="boostbox_display_speed" value="' . esc_attr( $display_speed ) . '" class="widefat" />';
+            $field .= '</div>';
+        
+            echo $field;
+        
+            $animations = array(
+                ''            => esc_attr__( '--', 'boostbox' ),
+                'fade-in'     => esc_attr__( 'Fade in', 'boostbox' ),
+                'slide-up'    => esc_attr__( 'Slide up', 'boostbox' ),
+                'slide-down'  => esc_attr__( 'Slide down', 'boostbox' ),
+                'slide-left'  => esc_attr__( 'Slide left', 'boostbox' ),
+                'slide-right' => esc_attr__( 'Slide right', 'boostbox' ),
+                'pop-swirl'   => esc_attr__( 'Pop swirl', 'boostbox' ),
+                'anvil'       => esc_attr__( 'Anvil', 'boostbox' ),
+            );
+        
+            $animations = apply_filters( 'boostbox_animation_type_animations', $animations );
+        
+            // Popup animation.
+            $popup_animation = get_post_meta( $post->ID, 'boostbox_animation_type', true );
+        
+            // Select an animation type: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Animation type', 'boostbox' ) . '</p>';
+            $field .= '<select id="boostbox_animation_type" name="boostbox_animation_type">';
+            // Loop through animations.
+            if ( ! empty( $animations ) ) {
+                foreach ( $animations as $animation => $value ) {
+                    if ( $animation == $popup_animation ) {
+                        $selected = 'selected="selected"';
+                    } else {
+                        $selected = '';
+                    }
+                    $field .= '<option value="' . esc_attr( $animation ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
+                }
             }
-            $field .= '<option value="' . esc_attr( $placement ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
-        }
-    }
-    $field .= '</select>';
-    $field .= '</div>';
+            $field .= '</select>';
+            $field .= '</div>';
+        
+            echo $field;
+        
+            // Popup animation.
+            $animation_speed = get_post_meta( $post->ID, 'boostbox_animation_speed', true );
+        
+            // Select an animation type: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Animation speed (milliseconds)', 'boostbox' ) . '</p>';
+            $field .= '<input type="number" name="boostbox_animation_speed" value="' . esc_attr( $animation_speed ) . '" class="widefat" />';
+            $field .= '</div>';
+        
+            echo $field;
+        
+            // Max width.
+            $max_width = get_post_meta( $post->ID, 'boostbox_display_max_width', true );
+        
+            // Max width: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Max width', 'boostbox' ) . '</p>';
+            $field .= '<input type="text" name="boostbox_display_max_width" value="' . esc_attr( $max_width ) . '" class="widefat" />';
+            $field .= '</div>';
+        
+            echo $field;
+        echo '</div>
 
-    echo $field;
+        <div id="second-panel" class="panel animated slideInRight">';
+            $placements = array(
+                'outside' => esc_attr__( 'Outside', 'boostbox' ),
+                'inside'  => esc_attr__( 'Inside', 'boostbox' ),
+                'hidden'  => esc_attr__( 'Hidden', 'boostbox' )
+            );
+        
+            $close_icon_placement = apply_filters( 'boostbox_close_icon_placements', $placements );
+        
+            // Icon placement.
+            $icon_placement = get_post_meta( $post->ID, 'boostbox_close_icon_placement', true );
+        
+            // Select close icon placement: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Close icon placement', 'boostbox' ) . '</p>';
+            $field .= '<select id="boostbox_close_icon_placement" name="boostbox_close_icon_placement">';
+            // Loop through animations.
+            if ( ! empty( $close_icon_placement ) ) {
+                foreach ( $close_icon_placement as $placement => $value ) {
+                    if ( $placement == $icon_placement ) {
+                        $selected = 'selected="selected"';
+                    } else {
+                        $selected = '';
+                    }
+                    $field .= '<option value="' . esc_attr( $placement ) . '" '. $selected .'>' . esc_html( $value ) . '</option>';
+                }
+            }
+            $field .= '</select>';
+            $field .= '</div>';
+        
+            echo $field;
+        
+            // Close color.
+            $close_color = get_post_meta( $post->ID, 'boostbox_close_icon_color', true );
+            // Set default color to #FFFFFF if no value is saved.
+            if ( empty( $close_color ) ) {
+                $close_color = '#FFFFFF';
+            }
+        
+            // Close Color: Build the field with WordPress color picker.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Close icon color', 'boostbox' ) . '</p>';
+            $field .= '<input type="text" class="boostbox-close-icon-color-picker" name="boostbox_close_icon_color" value="' . esc_attr( $close_color ) . '" />';
+            $field .= '</div>';
+        
+            echo $field;
+        echo '</div>
+    </div>
 
-    // Close color.
-    $close_color = get_post_meta( $post->ID, 'boostbox_close_icon_color', true );
-    // Set default color to #FFFFFF if no value is saved.
-    if ( empty( $close_color ) ) {
-        $close_color = '#FFFFFF';
-    }
-
-    // Close Color: Build the field with WordPress color picker.
-    $field  = '<div class="boostbox-field">';
-    $field .= '<p>' . esc_attr__( 'Close icon color', 'boostbox' ) . '</p>';
-    $field .= '<input type="text" class="boostbox-close-icon-color-picker" name="boostbox_close_icon_color" value="' . esc_attr( $close_color ) . '" />';
-    $field .= '</div>';
-
-    echo $field;
+</div>';
 }
 
 /**
