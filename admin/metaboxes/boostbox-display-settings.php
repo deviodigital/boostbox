333d262f6d30e1dd40673e2875e4abfa81321161
Robert DeVore <deviodigital@gmail.com>
2024-01-17 22:57:42 -0500

    :package: NEW: Added cookie days setting to popup display settings

--- admin/metaboxes/boostbox-display-settings.php
+++ admin/metaboxes/boostbox-display-settings.php
@@ -77,16 +77,16 @@ function boostbox_display_settings_metabox_content() {
                 'bottom-center' => esc_attr__( 'Bottom Center', 'boostbox' ),
                 'bottom-right'  => esc_attr__( 'Bottom Right', 'boostbox' ),
             );
-        
+
             $locations = apply_filters( 'boostbox_display_location_locations', $locations );
-        
+
             // Display location.
             $display_location = get_post_meta( $post->ID, 'boostbox_display_location', true );
-        
+
             if ( ! $display_location ) {
                 $display_location = 'center-center';
             }
-        
+
             // Select a popup location: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Display location', 'boostbox' ) . '</p>';
@@ -104,20 +104,20 @@ function boostbox_display_settings_metabox_content() {
             }
             $field .= '</select>';
             $field .= '</div>';
-        
+
             echo $field;
-        
+
             $triggers = array(
                 'auto-open'   => esc_attr__( 'Auto open', 'boostbox' ),
                 'on-scroll'   => esc_attr__( 'On Scroll', 'boostbox' ),
                 'time'        => esc_attr__( 'Time', 'boostbox' ),
             );
-        
+
             $triggers = apply_filters( 'boostbox_trigger_type_triggers', $triggers );
-        
+
             // Popup trigger.
             $trigger_type = get_post_meta( $post->ID, 'boostbox_trigger_type', true );
-        
+
             // Select an trigger type: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Trigger type', 'boostbox' ) . '</p>';
@@ -135,20 +135,20 @@ function boostbox_display_settings_metabox_content() {
             }
             $field .= '</select>';
             $field .= '</div>';
-        
+
             echo $field;
-        
+
             // Display speed.
             $display_speed = get_post_meta( $post->ID, 'boostbox_display_speed', true );
-        
+
             // Display Speed: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Display speed (milliseconds)', 'boostbox' ) . '</p>';
             $field .= '<input type="number" name="boostbox_display_speed" value="' . esc_attr( $display_speed ) . '" class="widefat" />';
             $field .= '</div>';
-        
+
             echo $field;
-        
+
             $animations = array(
                 ''            => esc_attr__( '--', 'boostbox' ),
                 'fade-in'     => esc_attr__( 'Fade in', 'boostbox' ),
@@ -159,12 +159,12 @@ function boostbox_display_settings_metabox_content() {
                 'pop-swirl'   => esc_attr__( 'Pop swirl', 'boostbox' ),
                 'anvil'       => esc_attr__( 'Anvil', 'boostbox' ),
             );
-        
+
             $animations = apply_filters( 'boostbox_animation_type_animations', $animations );
-        
+
             // Popup animation.
             $popup_animation = get_post_meta( $post->ID, 'boostbox_animation_type', true );
-        
+
             // Select an animation type: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Animation type', 'boostbox' ) . '</p>';
@@ -182,30 +182,42 @@ function boostbox_display_settings_metabox_content() {
             }
             $field .= '</select>';
             $field .= '</div>';
-        
+
             echo $field;
-        
+
             // Popup animation.
             $animation_speed = get_post_meta( $post->ID, 'boostbox_animation_speed', true );
-        
+
             // Select an animation type: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Animation speed (milliseconds)', 'boostbox' ) . '</p>';
             $field .= '<input type="number" name="boostbox_animation_speed" value="' . esc_attr( $animation_speed ) . '" class="widefat" />';
             $field .= '</div>';
-        
+
             echo $field;
-        
+
             // Max width.
             $max_width = get_post_meta( $post->ID, 'boostbox_display_max_width', true );
-        
+
             // Max width: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Max width', 'boostbox' ) . '</p>';
             $field .= '<input type="text" name="boostbox_display_max_width" value="' . esc_attr( $max_width ) . '" class="widefat" />';
             $field .= '</div>';
-        
+
             echo $field;
+
+            // Cookie Days.
+            $cookie_days = get_post_meta( $post->ID, 'boostbox_cookie_days', true );
+
+            // Cookie Days: Build the field.
+            $field  = '<div class="boostbox-field">';
+            $field .= '<p>' . esc_attr__( 'Cookie days (overrides global setting)', 'boostbox' ) . '</p>';
+            $field .= '<input type="number" name="boostbox_cookie_days" value="' . esc_attr( $cookie_days ) . '" class="widefat" />';
+            $field .= '</div>';
+
+            echo $field;
+
         echo '</div>
 
         <div id="second-panel" class="panel animated slideInRight">';
@@ -216,10 +228,10 @@ function boostbox_display_settings_metabox_content() {
             );
         
             $close_icon_placement = apply_filters( 'boostbox_close_icon_placements', $placements );
-        
+
             // Icon placement.
             $icon_placement = get_post_meta( $post->ID, 'boostbox_close_icon_placement', true );
-        
+
             // Select close icon placement: Build the field.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Close icon placement', 'boostbox' ) . '</p>';
@@ -237,22 +249,22 @@ function boostbox_display_settings_metabox_content() {
             }
             $field .= '</select>';
             $field .= '</div>';
-        
+
             echo $field;
-        
+
             // Close color.
             $close_color = get_post_meta( $post->ID, 'boostbox_close_icon_color', true );
             // Set default color to #FFFFFF if no value is saved.
             if ( empty( $close_color ) ) {
                 $close_color = '#FFFFFF';
             }
-        
+
             // Close Color: Build the field with WordPress color picker.
             $field  = '<div class="boostbox-field">';
             $field .= '<p>' . esc_attr__( 'Close icon color', 'boostbox' ) . '</p>';
             $field .= '<input type="text" class="boostbox-close-icon-color-picker" name="boostbox_close_icon_color" value="' . esc_attr( $close_color ) . '" />';
             $field .= '</div>';
-        
+
             echo $field;
         echo '</div>
     </div>
@@ -292,6 +304,7 @@ function boostbox_display_settings_metabox_save( $post_id, $post ) {
     $settings_meta['boostbox_display_max_width']    = filter_input( INPUT_POST, 'boostbox_display_max_width' );
     $settings_meta['boostbox_close_icon_color']     = sanitize_hex_color( filter_input( INPUT_POST, 'boostbox_close_icon_color' ) );
     $settings_meta['boostbox_close_icon_placement'] = filter_input( INPUT_POST, 'boostbox_close_icon_placement' );
+    $settings_meta['boostbox_cookie_days']          = filter_input( INPUT_POST, 'boostbox_cookie_days' );
 
     // Save $settings_meta as metadata.
     foreach ( $settings_meta as $key => $value ) {
